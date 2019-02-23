480319c890c22931c114020e2f4ddafa27421cff
Caleb Tsosie <kasusaku@gmail.com>
2019-02-23 18:55:00 +0000

    Added form validations to model for Contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ActiveRecord::Base
-
+  
+  #Validate data for contact form to save to database
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
+    
 end
