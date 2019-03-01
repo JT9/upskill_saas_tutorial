bf12b7437f9e0296ed94d3ce9037cc4c527e4888
Caleb Tsosie <kasusaku@gmail.com>
2019-03-01 22:54:48 +0000

    Added sign up links to home page

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,4 @@
 class Plan < ActiveRecord::Base
-  #Basic and Pro plan can have many users
+  #Basic and Pro plan can have many users with Associations
   has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  #each user should belong to a plan for Basic and Pro from -> plan.rb
+  #each user should belong to a plan for Basic and Pro from -> plan.rb 
+  #(Associations)
   belongs_to :plan
 end
