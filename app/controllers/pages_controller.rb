ef3e88a9565d66c7aa73713a4f94c59bfe8c44ae
Caleb Tsosie <kasusaku@gmail.com>
2019-03-02 00:52:35 +0000

    Added plan vars and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
+  #GET request for / which is our home page
   #action name for corresponding folder for home in views/pages
   def home
+    #instance vars can be used inside view files
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   
   def about
